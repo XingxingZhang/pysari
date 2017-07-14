8efecb8226551a1f6aaa74c9c07246e7a316d3f0
XingxingZhang <zxxpku@gmail.com>
2017-07-14 21:28:57 +0100

    make sari a python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+""" Setup script for mlp package. """
+
+from setuptools import setup
+
+setup(
+    name = "sari",
+    author = "Wei Xu, Xingxing Zhang",
+    description = ("A new evaluation metric for sentence simplification "
+                   "SARI"),
+    url = "https://github.com/cocoxu/simplification",
+    packages=['sari']
+)
+
+
